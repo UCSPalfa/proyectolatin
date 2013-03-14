c1896e4b94d2459a5983860c6561560446880e87
Gladys Carrillo <gladys.carrillo@cti.espol.edu.ec>
2013-03-14 16:44:54 -0500

    cambiado el texbox para elegir la comunidad, se usa el TextListBox, por que el autocomplete luego de la selección mostraba el id en vez del nombre

--- mod/relatedgroups/actions/relatedgroups/add.php
+++ mod/relatedgroups/actions/relatedgroups/add.php
@@ -6,12 +6,14 @@ $group_guid = get_input('group');
 $othergroup_guid = get_input('othergroup');
 $othergroup_url = get_input('othergroup_url'); // maybe it isn't used
 $group = get_entity($group_guid);
-$othergroup = get_entity($othergroup_guid);
 
+$relgroups = explode(",",$othergroup_guid);
+foreach ($relgroups as $othergroup1){
+$othergroup = get_entity($othergroup1);
 if(!$othergroup && $othergroup = relatedgroups_get_group_from_url($othergroup_url)){
 	$othergroup_guid = $othergroup->guid;
 }
-
+$othergroup_guid = $othergroup->guid;
 if ($group instanceof ElggGroup && $group->canEdit() && $othergroup instanceof ElggGroup) {
 	if (!check_entity_relationship($group_guid, 'related', $othergroup_guid) && $group_guid != $othergroup_guid) {
 		add_entity_relationship($group_guid, 'related', $othergroup_guid);
@@ -19,5 +21,6 @@ if ($group instanceof ElggGroup && $group->canEdit() && $othergroup instanceof E
 }
 else{
 	register_error(elgg_echo('relatedgroups:add:error'));
+}
 }
 forward(REFERER);
